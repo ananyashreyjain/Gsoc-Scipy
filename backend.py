66fb453ad32933b23bed6d035790ae3e27ea7a71
ananyashreyjain <31594632+ananyashreyjain@users.noreply.github.com>
2019-04-16 00:40:30 +0530

    Create backend.py

---
+++ backend.py
@@ -0,0 +1,41 @@
+import pyfftw as FFTW
+
+lib = {"library":None}
+
+class backend:
+
+    def __init__(self, library):
+    
+        if(library == "pyfftw"):
+            self.backend_library = FFTW
+            
+    def set_args(self, arg1, arg2):
+        
+        self.arg_dict["arg1"]=arg1
+        self.arg_dict["arg2"]=arg2
+        lib["arg1"]=arg1
+        lib["arg2"]=arg2
+            
+    def __enter__(self):
+    
+        """
+        Code for changing the backend
+        
+        """
+        self.arg_dict = {"arg1":"default planning_effort",
+                         "arg2":"default threads"}
+        lib["library"] = self.backend_library
+
+        return self
+        
+    def __exit__(self, type, value, traceback):
+        
+        """
+        Changing things back to default
+        
+        """
+    
+        lib["library"] = None
+        del lib["arg1"]
+        del lib["arg2"]
+   
